fbb8b5b0eefd3b8578805024085e04e2dc8cdb7e
anderson <andersonrochatavares@gmail.com>
2019-07-29 16:40:13 -0300

    various fixes

--- client.py
+++ client.py
@@ -1,7 +1,11 @@
+#!/usr/bin/python3
+
 import os.path
+import sys
 import time
 from pathlib import Path
 import subprocess
+
 
 class Client(object):
 
@@ -16,17 +20,9 @@ class Client(object):
         self.done = os.path.join(basedir, "done.txt")
         self.lock_file = os.path.join(basedir, ".lock")
 
-
     def run(self):
 
         while True:
-
-
-            # skips if the directory is locked
-            if os.path.exists(self.lock_file):
-                time.sleep(1)
-                continue
-
             # if job is currently running, checks if it has finished
             if self.job is not None:
                 if self.job.poll() is not None: # terminated!
@@ -39,26 +35,34 @@ class Client(object):
 
             # looks for a job
             else:
-                self.job = self.find_job()
+                self.find_job()
 
-
+                if self.job is None:
+                    print("No job found. Sleeping for 5 seconds...")
+                    time.sleep(5)
 
     def find_job(self):
-
         self.lock()
 
         # grabs a job
         todo_handler = open(self.todo, 'r')
-        if todo_handler.readline(4096) is None or todo_handler.readline() == '':
-            todo_handler.close()
-            return None # no job in file
+        first_line = todo_handler.readline(4096).strip()
+        if first_line is not None and first_line != '':
 
-        # grabs the job, starts it and returns it
-        subprocess.Popen(
-            './rlexperiment.sh -c %s -o %s/rep%s %s' \
-            % (experiment_config, output_prefix, str(total_runs).zfill(2), additional_args),
-            shell=True
-        )
+            # there's a valid job in the file, retrieves and runs it
+            self.job_command = first_line
+
+            print("Starting job '%s'" % self.job_command)
+
+            # grabs the job, starts it and returns it
+            self.job = subprocess.Popen(
+                self.job_command,
+                shell=True
+            )
+
+        todo_handler.close()
+        if self.job is not None:  # if I found a job, move it to in progress
+            self.move(self.job_command, self.todo, self.in_progress)
 
         self.unlock()
 
@@ -73,22 +77,26 @@ class Client(object):
     # moves a line from file1 to file2
     def move(self, line_to_move, file_name1, file_name2):
 
+        #print('Moving %s from %s to %s' % (line_to_move, file_name1, file_name2))
+
         # removes the line in file1 (opens, removes, rewrites)
         read_handler = open(file_name1, 'r')
-        to_remove = [line.trim() for line in read_handler.readlines()]
+        to_remove = [line.strip() for line in read_handler.readlines()]
+        #print('%s' % line_to_move, to_remove)
         to_remove.remove(line_to_move)
         read_handler.close()
 
         rewrite_handler = open(file_name1, 'w')
-        rewrite_handler.writelines(line_to_move)
+        rewrite_handler.writelines(to_remove)
         rewrite_handler.close()
 
         # appends the line to file2
         file2_handler = open(file_name2, 'a')
-        file2_handler.writelines([line_to_move])
+        file2_handler.write('%s\n' % line_to_move)
 
     def lock(self):
         while os.path.exists(self.lock_file): # waits until the directory is free
+            print("Directory is locked. ")
             time.sleep(1)
             continue
 
@@ -99,4 +107,7 @@ class Client(object):
         os.remove(self.lock_file)
 
 
-
+if __name__ == '__main__':
+    print("Starting simple file job client.")
+    client = Client(sys.argv[1])
+    client.run()
